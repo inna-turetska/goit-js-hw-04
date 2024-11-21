551c6bad56828deb5a76482c0d847dd80005130b
Inna Turetska <innaturetska1987@gmail.com>
2024-11-21 12:32:45 +0200

    task 4

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,26 @@
-function slugify(title) {
-    let slug = title.toLowerCase().split(' ')
-    return slug.join("-") 
-}
-console.log(slugify("Arrays for beginners")); 
-console.log(slugify("English for developer")); 
-console.log(slugify("Ten secrets of JavaScript")); 
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
+function isEnoughCapacity(products, containerSize) {
+    let sum = 0;
+    const productsValues = Object.values(products)
+    for (const value of productsValues ){
+      sum += value
+    }
+    return sum <= containerSize
+    }  
+    
+        
+    
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,26 @@
-function filterArray(numbers, value) {
-    let newNumber = []; 
-    
-   
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] > value) { 
-            newNumber.push(numbers[i]); 
-        }
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+ 
+    changeUsername(newName) {
+        this.username = newName;
+    },
+
+    updatePlayTime(hours){
+      this.playTime += hours;
+    },
+        
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`
+    },
     }
-    return newNumber;
-}
-console.log(filterArray([1, 2, 3, 4, 5], 3)); 
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5)); 
-console.log(filterArray([12, 24, 8, 41, 76], 38)); 
-console.log(filterArray([12, 24, 8, 41, 76], 20)); 
+
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
